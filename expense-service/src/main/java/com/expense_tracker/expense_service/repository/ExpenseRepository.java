791031be9236a4c76be02749d5ced2b0d3a47547
Skew008 <shreyanshtelrandhe234@gmail.com>
2025-06-23 22:13:50 +0530

    Update Expense Service for Goal Service

--- expense-service/src/main/java/com/expense_tracker/expense_service/repository/ExpenseRepository.java
+++ expense-service/src/main/java/com/expense_tracker/expense_service/repository/ExpenseRepository.java
@@ -2,8 +2,12 @@ package com.expense_tracker.expense_service.repository;
 
 import com.expense_tracker.expense_service.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,4 +16,17 @@ import java.util.UUID;
 public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
     Optional<Expense> findByEmail(String email);
     List<Expense> findAllByEmail(String email);
+
+    @Query(value = """
+    SELECT COALESCE(SUM(amount), 0)
+    FROM expense
+    WHERE email = :email AND date BETWEEN :from AND :to
+    AND (:category IS NULL OR category = :category)
+""", nativeQuery = true)
+    Double getTotalByDateRangeAndCategory(
+            @Param("email") String email,
+            @Param("from") LocalDate from,
+            @Param("to") LocalDate to,
+            @Param("category") String category
+    );
 }
